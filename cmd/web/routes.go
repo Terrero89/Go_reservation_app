a0d90eaca6367d45a9bc9041a4ba996f45127823
Terrero <sergio-terrero@hotmail.com>
2023-05-18 21:18:32 -0400

    starting to work on pages templtes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,5 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	//creating a file server to get images and other docs
+	fileServer := http.FileServer(http.Dir("./static/images")) //created file with where and name of the directory
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer)) // select all object or files within the static a
 	return mux
 }
